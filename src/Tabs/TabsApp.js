92957ea33abcdaf5077a77861c1fea7a86128c7d
Marco Jovanovic <klaudiusmarkus@gmail.com>
2021-02-18 18:09:25 +0100

    dodat loading

--- src/Tabs/TabsApp.js
+++ src/Tabs/TabsApp.js
@@ -6,22 +6,25 @@ const url = 'https://course-api.com/react-tabs-project';
 
 function TabsApp() {
   const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const [tabsInfo, setTabsInfo] = useState([]);
 
+  const getTabInformation = async () => {
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    setTabsInfo(data);
+  };
+
   useEffect(() => {
-    const getTabInformation = async () => {
-      const res = await fetch(url);
-
-      const data = await res.json();
-
-      setTabsInfo(data[index]);
-    };
-
     getTabInformation();
-  }, [index]);
 
-  console.log(tabsInfo.duties);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+  }, [index]);
 
   const setButtonIndex = () => {
     setIndex((oldIndex) => {
@@ -35,31 +38,39 @@ function TabsApp() {
     });
   };
 
-  return (
-    <>
-      <h1 className="tabs__main--title">Expierence</h1>
-      <div className="tabs">
-        <div className="tabs__buttons">
-          <button onClick={setButtonIndex} className="btn">
-            Cukker
-          </button>
-          <button onClick={setButtonIndex} className="btn">
-            Tommy
-          </button>
-          <button onClick={setButtonIndex} className="btn">
-            BigDrop
-          </button>
+  if (loading) {
+    const { company, title, dates, duties } = tabsInfo[index];
+
+    return (
+      <>
+        <h1 className="tabs__main--title">Expierence</h1>
+        <div className="tabs">
+          <div className="tabs__buttons">
+            <button onClick={setButtonIndex} className="btn">
+              Cukker
+            </button>
+            <button onClick={setButtonIndex} className="btn">
+              Tommy
+            </button>
+            <button onClick={setButtonIndex} className="btn">
+              BigDrop
+            </button>
+          </div>
+          <div className="tabs__main">
+            <h2>{title}</h2>
+            <button className="tabs__main--btn">{company}</button>
+            <h3>{dates}</h3>
+            {duties.map((item) => {
+              return <p>{item}</p>;
+            })}
+          </div>
+          ;
         </div>
-        <div className="tabs__main">
-          <h2>{tabsInfo.title}</h2>
-          <button className="tabs__main--btn">{tabsInfo.company}</button>
-          <h3>{tabsInfo.dates}</h3>
-          <p>{tabsInfo.duties}</p>
-        </div>
-        ;
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return <h1 className="text-center">Loading ...</h1>;
+  }
 }
 
 export default TabsApp;
